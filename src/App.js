d981c6a7571ca13b6a8b7359a3df90fa19d0d32c
HumbertoBernal <Hdbernal1>
2020-10-21 09:06:57 -0500

    Index 0.1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Projecto Interdisciplinario 3: Melanoma Identifier
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click to continue
         </a>
       </header>
     </div>
