11d1377dacec1b9a9f3196df708a7d9ddddff2d8
HumbertoBernal <Hdbernal1>
2020-12-07 17:42:55 -0500

    imagebox add

---
+++ src/components/ImageBox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Box } from '@chakra-ui/core';
+import Image from "@chakra-ui/core/dist/Image";
+
+const ImageBox = (props) => 
+<React.Fragment>
+    <Box p={"5"} pt={"4"} m={"2"} mr={"20"} overflow={"hidden"} rounded={"lg"} borderWidth={"1px"}>
+        <Box
+            p={"1"}
+            fontWeight="semibold"
+            lineHeight="tight"
+            bg={"tomato"}
+            rounded={"md"}
+            mb={"1"}
+            >
+            {props.titulo}
+        </Box>
+        <Image src={props.url} w={"300px"} h={"300px"}/>
+   </Box>
+
+</React.Fragment>
+
+export default ImageBox;
